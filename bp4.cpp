a932407a36370e52e054a3e426541da7d6c319a6
Anushka Singh <anushkasingh117733@gmail.com>
2022-06-22 17:59:24 +0530

    Update bp4.cpp

--- bp4.cpp
+++ bp4.cpp
@@ -37,3 +37,41 @@ void minimumSteps(int ar1[], int ar2[], int n){
     minimumSteps(ar1, ar2, n);
 
     return 0;}
+
+
+
+
+
+
+
+Problem
+You are given two arrays a1....an and b1...bn . In each step, you can set ai = ai-bi if ai >_bi. Determine the minimum number of steps that are required to make all 's equal.
+
+Input format
+
+First line:  n
+Second line: a1..an
+Third line: b1..bn
+Output format
+
+Print the minimum number of steps that are required to make all 's equal. If it is not possible, then print -1.
+
+Constraints
+
+
+Sample input
+
+2
+5 6
+4 3
+
+Sample output
+
+-1
+
+Sample Input
+5
+5 7 10 5 15
+2 2 1 3 5
+Sample Output
+8
