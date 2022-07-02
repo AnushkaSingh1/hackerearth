dfe76d6ac2f17b63db695daac245f5c9081682fc
Anushka Singh <anushkasingh117733@gmail.com>
2022-07-02 23:19:50 +0530

    Create bp14.cpp

---
+++ bp14.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int N,fact=1;
+
+    cin>>N;
+
+    for(int i=1;i<=N;i++)
+
+    fact=fact*i;
+
+cout<<fact;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+int n,r=1;
+
+cin>>n;
+
+for(int i=1;i<=n;i++)
+
+{
+
+r=r*i;
+
+}
+
+cout<<r<<endl;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+You have been given a positive integer N. You need to find and print the Factorial of this number. The Factorial of a positive integer N refers to the product of all number in the range from 1 to N. You can read more about the factorial of a number here.
+
+Input Format:
+The first and only line of the input contains a single integer N denoting the number whose factorial you need to find.
+
+Output Format
+Output a single line denoting the factorial of the number N.
+
+Constraints
+
+Sample Input
+2
+Sample Output
+2
+Time Limit: 1
+Memory Limit: 256
+Source Limit:
+
