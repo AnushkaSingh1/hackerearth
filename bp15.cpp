0d86a180f694b3203ac7c49168123b0374a024cd
Anushka Singh <anushkasingh117733@gmail.com>
2022-07-03 23:30:47 +0530

    Create bp15.cpp

---
+++ bp15.cpp
@@ -0,0 +1,114 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int n;
+
+    while(true)
+
+    {
+
+        cin>>n;
+
+        if(n==42) break;
+
+        cout<<n<<endl;
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+
+
+ 
+
+using namespace std;
+
+
+ 
+
+int main() {
+
+    int num;
+
+    for(;;){
+
+    cin>> num;
+
+    if(num!=42)
+
+    {
+
+        cout<<num<<endl;
+
+        
+
+    }
+
+    else
+
+    {
+
+        exit(0);
+
+    }
+
+    }
+
+
+ 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Your program is to use the brute-force approach in order to find the Answer to Life, the Universe, and Everything. More precisely... rewrite small numbers from input to output. Stop processing input after reading in the number 42. All numbers at input are integers of one or two digits.
+
+Sample Input
+1
+2
+88
+42
+99
+Sample Output
+1
+2
+88
+Time Limit: 5
+Memory Limit: 256
+Source Limit:
+
+
+
+
+
+
+
+
